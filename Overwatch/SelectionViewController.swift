c3bb12ac41676d01507f79f79563134cbb445f70
Lloyd1nSpace <lloyd1nspace@icloud.com>
2017-01-31 11:32:55 -0500

    Getting closer still working on it

--- Overwatch/SelectionViewController.swift
+++ Overwatch/SelectionViewController.swift
@@ -15,7 +15,15 @@ class SelectionViewController: UIViewController {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var heroNameLabel: UILabel!
     @IBOutlet weak var heroScrollView: UIScrollView!
+    
+    var offenseHeroImages = [UIImage]()
+    var defenseHeroImages = [UIImage]()
+    var tankHeroImages = [UIImage]()
+    var supportHeroImages = [UIImage]()
+    
     var characterImageView: UIImageView!
+    
+    
     
     var game: Game = Game()
     var heroType: HeroType! {
@@ -51,58 +59,89 @@ class SelectionViewController: UIViewController {
         // Tank & Support have 5 characters
         // For each heroType I need to: adjust the widthConstraint of the stack view, addArrangedSubviews of UIImageViews corressponding with heroType as well as removeArrangedSubviews of previous heroes
         // Using the scrollViewDelegate, I need to also update the labels to correspond with the UIImageViews
-    
         
-        
-            if heroType == .offense {
-                  for offenseHero in game.offenseCharacters {
+
+        if heroType == .offense {
+            for offenseHero in game.offenseCharacters {
+                //                    characterImageView.removeFromSuperview()
                 
-//                    characterImageView.removeFromSuperview()
-                    heroNameLabel.text = offenseHero.name.description
-                    characterImageView = UIImageView(image: offenseHero.produceProfileImage())
-                    characterStackView.addArrangedSubview(characterImageView)
-                    characterStackViewWidthConstraint.constant *= 6
-                }
-            } else if heroType == .defense {
-                for defenseHero in game.defenseCharacters {
-                    characterImageView.removeFromSuperview()
-                    characterImageView = UIImageView(image: defenseHero.produceProfileImage())
-                    characterStackView.addArrangedSubview(characterImageView)
-                    characterStackViewWidthConstraint.constant *= 6
-                }
-            } else if heroType == .tank {
-                for tankHero in game.tankCharacters {
-                    characterImageView.removeFromSuperview()
-                    characterImageView = UIImageView(image: tankHero.produceProfileImage())
-                    characterStackView.addArrangedSubview(characterImageView)
-                    characterStackViewWidthConstraint.constant *= 4
-                }
-            } else if heroType == .support {
-                for supportHero in game.supportCharacters {
-                    characterImageView.removeFromSuperview()
-                    characterImageView = UIImageView(image: supportHero.produceProfileImage())
-                    characterStackView.addArrangedSubview(characterImageView)
-                    characterStackViewWidthConstraint.constant *= 4
-                }
+                offenseHeroImages.append(offenseHero.produceProfileImage())
+                
+                
+                heroNameLabel.text = offenseHero.name.description
+                characterImageView = UIImageView(image: offenseHero.produceProfileImage())
+//                characterStackView.addArrangedSubview(characterImageView)
+                characterStackViewWidthConstraint.constant *= 6
+//                imageViewConstraints()
+            }
+        } else if heroType == .defense {
+            for defenseHero in game.defenseCharacters {
+                
+                
+                defenseHeroImages.append(defenseHero.produceProfileImage())
+                
+                
+//                characterImageView.removeFromSuperview()
+                characterStackView.removeArrangedSubview(characterImageView)
+                heroNameLabel.text = defenseHero.name.description
+                characterImageView = UIImageView(image: defenseHero.produceProfileImage())
+                characterStackView.addArrangedSubview(characterImageView)
+                characterStackViewWidthConstraint.constant *= 6
+                imageViewConstraints()
+            }
+        } else if heroType == .tank {
+            for tankHero in game.tankCharacters {
+                
+                tankHeroImages.append(tankHero.produceProfileImage())
+                
+                
+//                characterImageView.removeFromSuperview()
+                characterStackView.removeArrangedSubview(characterImageView)
+                heroNameLabel.text = tankHero.name.description
+                characterImageView = UIImageView(image: tankHero.produceProfileImage())
+                characterStackView.addArrangedSubview(characterImageView)
+                characterStackViewWidthConstraint.constant *= 4
+                imageViewConstraints()
+            }
+        } else if heroType == .support {
+            for supportHero in game.supportCharacters {
+                
+                supportHeroImages.append(supportHero.produceProfileImage())
+                
+                
+//                characterImageView.removeFromSuperview()
+                characterStackView.removeArrangedSubview(characterImageView)
+                heroNameLabel.text = supportHero.name.description
+                characterImageView = UIImageView(image: supportHero.produceProfileImage())
+                characterStackView.addArrangedSubview(characterImageView)
+                characterStackViewWidthConstraint.constant *= 4
+                imageViewConstraints()
+            }
         }
         
-        imageViewConstraints()
-
+        
+        
+        
+        
+        
     }
     
     
     func imageViewConstraints() {
         characterImageView.translatesAutoresizingMaskIntoConstraints = false
-        characterImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
-        characterImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
-        characterImageView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
-        characterImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        characterImageView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        characterImageView.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
+        
+        //        characterImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        //        characterImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        //        characterImageView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        //        characterImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
     }
     
 }
 
 extension SelectionViewController: UIScrollViewDelegate {
-
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
     }
@@ -120,6 +159,9 @@ extension SelectionViewController: UIScrollViewDelegate {
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        
+        
         
     }
     
